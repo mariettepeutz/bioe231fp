b5792cb8a4f8855a81c6074db4a2ebb9ca505425
Kate Barouch <131073205+katebarouch@users.noreply.github.com>
2024-11-24 17:33:01 -0800

    Update main.sh
    dengue

--- main.sh
+++ main.sh
@@ -1,12 +1,6 @@
 ### Dengue JBrowse Database Code ###
 
-# create variables
-FASTA_URL="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.fna.gz"
-GFF3_URL="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.gff.gz"
-GENOME_FILE="viral_genome.fa"
-GFF3_FILE="genes.gff"
-
-# Helper function for error handling - this is important so that we exit if something goes wrong!
+# Helper function for error handling
 check_error() {
     if [ $? -ne 0 ]; then
         echo "Error encountered in previous command. Exiting."
@@ -14,9 +8,9 @@ check_error() {
     fi
 }
 
-# Download and process main dengue genome (DENV-2 with V91A mutation)
+# Download and process the main genome (DENV-2 with V91A mutation)
 echo "Downloading Dengue genome..."
-wget -q "$FASTA_URL" -O genome.fna.gz
+wget -q "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.fna.gz" -O genome.fna.gz
 check_error
 
 echo "Decompressing genome file..."
@@ -24,20 +18,20 @@ gunzip genome.fna.gz
 check_error
 
 echo "Renaming genome file..."
-mv genome.fna "$GENOME_FILE"
+mv genome.fna viral_genome.fa
 check_error
 
 echo "Indexing genome file with samtools..."
-samtools faidx "$GENOME_FILE"
+samtools faidx viral_genome.fa
 check_error
 
 echo "Adding genome assembly to JBrowse..."
-jbrowse add-assembly "$GENOME_FILE" --out "$APACHE_ROOT/jbrowse2" --load copy
+jbrowse add-assembly viral_genome.fa --out /path/to/your/apache/root/jbrowse2 --load copy
 check_error
 
 # Download and process annotations
 echo "Downloading genome annotations..."
-wget -q "$GFF3_URL" -O annotations.gff.gz
+wget -q "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.gff.gz" -O annotations.gff.gz
 check_error
 
 echo "Decompressing annotations file..."
@@ -45,19 +39,19 @@ gunzip annotations.gff.gz
 check_error
 
 echo "Sorting GFF3 annotations..."
-jbrowse sort-gff annotations.gff > "$GFF3_FILE"
+jbrowse sort-gff annotations.gff > genes.gff
 check_error
 
 echo "Compressing sorted GFF3 file..."
-bgzip "$GFF3_FILE"
+bgzip genes.gff
 check_error
 
 echo "Indexing compressed GFF3 file with tabix..."
-tabix "${GFF3_FILE}.gz"
+tabix genes.gff.gz
 check_error
 
 echo "Adding annotations track to JBrowse..."
-jbrowse add-track "${GFF3_FILE}.gz" --out "$APACHE_ROOT/jbrowse2" --load copy
+jbrowse add-track genes.gff.gz --out /path/to/your/apache/root/jbrowse2 --load copy
 check_error
 
 echo "Genome and annotations successfully added to JBrowse for main Dengue genome (DENV-2 with V91A mutation)."

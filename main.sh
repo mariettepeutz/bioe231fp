8fc59e61f5b3c0fe7059472c98031fbb07ff58d9
Kate Barouch <131073205+katebarouch@users.noreply.github.com>
2024-11-27 14:37:40 -0800

    Update main.sh
    added function logic and a main script execution section

--- main.sh
+++ main.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-### Dengue ###
-
-#!/bin/bash
+### Dengue Alignments Tracks ###
 
 # Helper function for error handling
 check_error() {
@@ -13,7 +11,6 @@ check_error() {
 }
 
 ### Check Directory and APACHE_ROOT ###
-
 WORKDIR=$(pwd)
 
 # Check if APACHE_ROOT is defined
@@ -34,82 +31,147 @@ if [ ! -d "$APACHE_ROOT/jbrowse2" ]; then
     exit 1
 fi
 
-### Download and Process the Reference Genome ###
-echo "Downloading Dengue genome (reference genome)..."
-wget -q "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.fna.gz" -O $WORKDIR/genome.fna.gz
-check_error
+# Function to process a reference genome
+process_reference_genome() {
+    virus_name="$1"
+    genome_url="$2"
+    echo "Processing $virus_name reference genome..."
+    wget -q "$genome_url" -O "$WORKDIR/${virus_name}_genome.fna.gz"
+    check_error
 
-echo "Decompressing reference genome file..."
-gunzip -f $WORKDIR/genome.fna.gz
-check_error
+    echo "Unzipping $virus_name reference genome..."
+    gunzip -f "$WORKDIR/${virus_name}_genome.fna.gz"
+    check_error
 
-echo "Renaming reference genome file..."
-mv $WORKDIR/genome.fna $WORKDIR/viral_genome.fa
-check_error
+    echo "Indexing $virus_name reference genome..."
+    samtools faidx "$WORKDIR/${virus_name}_genome.fna"
+    check_error
 
-echo "Indexing reference genome file with samtools..."
-samtools faidx $WORKDIR/viral_genome.fa
-check_error
+    echo "Adding $virus_name reference genome to JBrowse..."
+    jbrowse add-assembly "$WORKDIR/${virus_name}_genome.fna" --out "$APACHE_ROOT/jbrowse2" --load copy
+    check_error
+}
 
-echo "Adding single-stranded RNA reference genome assembly to JBrowse..."
-jbrowse add-assembly $WORKDIR/viral_genome.fa --out $APACHE_ROOT/jbrowse2 --load copy
-check_error
+# Function to process a reference annotation
+process_reference_annotation() {
+    virus_name="$1"
+    annotation_url="$2"
+    echo "Processing $virus_name annotations..."
+    wget -q "$annotation_url" -O "$WORKDIR/${virus_name}_annotations.gff.gz"
+    check_error
 
-### Download and Process Annotations ###
-echo "Downloading genome annotations..."
-wget -q "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.gff.gz" -O $WORKDIR/annotations.gff.gz
-check_error
+    echo "Unzipping $virus_name annotations..."
+    gunzip -f "$WORKDIR/${virus_name}_annotations.gff.gz"
+    check_error
 
-echo "Decompressing annotations file..."
-gunzip -f $WORKDIR/annotations.gff.gz
-check_error
+    echo "Sorting $virus_name annotations..."
+    jbrowse sort-gff "$WORKDIR/${virus_name}_annotations.gff" > "$WORKDIR/${virus_name}_genes.gff"
+    check_error
 
-echo "Sorting GFF3 annotations..."
-jbrowse sort-gff $WORKDIR/annotations.gff > $WORKDIR/genes.gff
-check_error
+    echo "Compressing $virus_name annotations..."
+    bgzip -f "$WORKDIR/${virus_name}_genes.gff"
+    check_error
 
-echo "Compressing sorted GFF3 file..."
-bgzip -f $WORKDIR/genes.gff
-check_error
+    echo "Indexing $virus_name annotations..."
+    tabix "$WORKDIR/${virus_name}_genes.gff.gz"
+    check_error
 
-echo "Indexing compressed GFF3 file with tabix..."
-tabix $WORKDIR/genes.gff.gz
-check_error
+    echo "Adding $virus_name annotations to JBrowse..."
+    jbrowse add-track "$WORKDIR/${virus_name}_genes.gff.gz" --out "$APACHE_ROOT/jbrowse2" --load copy
+    check_error
+}
 
-echo "Adding annotations track to JBrowse..."
-jbrowse add-track $WORKDIR/genes.gff.gz --out $APACHE_ROOT/jbrowse2 --load copy
-check_error
+# Function to process comparison genome for alignment track
+process_comparison_genome() {
+    reference_virus_name="$1"
+    comparison_virus_name="$2"
+    comparison_genome_url="$3"
 
-echo "Reference dengue genome and annotations successfully added to JBrowse."
+    echo "Building Bowtie2 index for $reference_virus_name genome..."
+    bowtie2-build "$WORKDIR/${reference_virus_name}_genome.fna" "$WORKDIR/${reference_virus_name}_genome_index"
+    check_error
 
-### Align Comparison Genome to Reference Genome ###
+    echo "Downloading $comparison_virus_name..."
+    wget -q "$comparison_genome_url" -O "$WORKDIR/${comparison_virus_name}.fa"
+    check_error
 
-echo "Preparing reference genome..."
-bowtie2-build $WORKDIR/viral_genome.fa $WORKDIR/viral_genome
-check_error
+    echo "Aligning $comparison_virus_name to $reference_virus_name reference genome..."
+    bowtie2 -x "$WORKDIR/${reference_virus_name}_genome_index" -f "$WORKDIR/${comparison_virus_name}.fa" -S "$WORKDIR/${comparison_virus_name}.sam"
+    check_error
 
-echo "Downloading and decompressing comparison genome..."
-wget "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=OR486055.1&report=fasta&format=text" -O dengue_virus1.fa
-check_error 
+    echo "Converting $comparison_virus_name alignment to BAM format..."
+    samtools view -bS "$WORKDIR/${comparison_virus_name}.sam" > "$WORKDIR/${comparison_virus_name}.bam"
+    check_error
 
-echo "Aligning comparison genome to reference genome using Bowtie2..."
-bowtie2 -x $WORKDIR/viral_genome -f $WORKDIR/dengue_virus1.fa -S $WORKDIR/comparison_genome.sam
-check_error 
+    echo "Sorting $comparison_virus_name BAM file..."
+    samtools sort "$WORKDIR/${comparison_virus_name}.bam" -o "$WORKDIR/${comparison_virus_name}.sorted.bam"
+    check_error
 
-echo "Converting SAM file to BAM file..."
-samtools view -bS $WORKDIR/comparison_genome.sam > $WORKDIR/comparison_genome.bam
-check_error 
+    echo "Indexing $comparison_virus_name sorted BAM file..."
+    samtools index "$WORKDIR/${comparison_virus_name}.sorted.bam"
+    check_error
 
-echo "Sorting BAM file..."
-samtools sort $WORKDIR/comparison_genome.bam -o $WORKDIR/comparison_genome.sorted.bam
-check_error 
+    echo "Adding $comparison_virus_name alignment track to JBrowse..."
+    jbrowse add-track "$WORKDIR/${comparison_virus_name}.sorted.bam" --out "$APACHE_ROOT/jbrowse2" --load copy --name "$comparison_virus_name"
+    check_error
+}
 
-echo "Indexing BAM file..."
-samtools index $WORKDIR/comparison_genome.sorted.bam
-check_error 
+### Main Script Execution ###
 
-echo "Adding alignment track to JBrowse..."
-jbrowse add-track $WORKDIR/comparison_genome.sorted.bam --out $APACHE_ROOT/jbrowse2 --load copy
-check_error 
+# Process DENV-1 Genomes
+process_reference_genome "DENV-1" "https://ftp.ncbi..."
+process_reference_annotation "DENV-1" "https://ftp.ncbi..."
 
-echo "Comparison genome alignment successfully added to JBrowse."
+process_comparison_genome "DENV-1" \
+    "https://ftp.ncbi...." \
+    "Variant1" "https://..."
+process_comparison_genome "DENV-1" \
+    "https://ftp.ncbi...." \
+    "Variant2" "https://..."
+process_comparison_genome "DENV-1" \
+    "https://ftp.ncbi...." \
+    "Variant3" "https://..."
+
+# Process DENV-2 Genomes
+process_reference_genome "DENV-2" "https://ftp.ncbi..."
+process_reference_annotation "DENV-2" "https://ftp.ncbi..."
+
+process_comparison_genome "DENV-2" \
+    "https://ftp.ncbi...." \
+    "Variant1" "https://..."
+process_comparison_genome "DENV-2" \
+    "https://ftp.ncbi...." \
+    "Variant2" "https://..."
+process_comparison_genome "DENV-2" \
+    "https://ftp.ncbi...." \
+    "Variant3" "https://..."
+
+# Process DENV-3 Genomes
+process_reference_genome "DENV-3" "https://ftp.ncbi..."
+process_reference_annotation "DENV-3" "https://ftp.ncbi..."
+
+process_comparison_genome "DENV-3" \
+    "https://ftp.ncbi...." \
+    "Variant1" "https://..."
+process_comparison_genome "DENV-3" \
+    "https://ftp.ncbi...." \
+    "Variant2" "https://..."
+process_comparison_genome "DENV-3" \
+    "https://ftp.ncbi...." \
+    "Variant3" "https://..."
+
+# Process DENV-4 Genomes
+process_reference_genome "DENV-4" "https://ftp.ncbi..."
+process_reference_annotation "DENV-4" "https://ftp.ncbi..."
+
+process_comparison_genome "DENV-4" \
+    "https://ftp.ncbi...." \
+    "Variant1" "https://..."
+process_comparison_genome "DENV-4" \
+    "https://ftp.ncbi...." \
+    "Variant2" "https://..."
+process_comparison_genome "DENV-4" \
+    "https://ftp.ncbi...." \
+    "Variant3" "https://..."
+
+echo "All reference genomes, annotations, and comparison genomes have been processed and added to JBrowse."

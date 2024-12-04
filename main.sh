bb9c7d79e358de5353fb72adcf4b7dde487eeeb5
Kate Barouch <131073205+katebarouch@users.noreply.github.com>
2024-12-03 18:16:20 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -143,7 +143,7 @@ process_comparison_genome() {
 
 # Process DENV-1 Genomes
 process_reference_genome "DENV-1" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.fna.gz" 
-process_reference_annotation_file "DENV-1" "denv1genes.gff3"
+process_reference_annotation_file "DENV-1" "referencedenv1genes.gff3"
 process_reference_annotation "DENV-1" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.gff.gz"
 
 process_comparison_genome "DENV-1" \

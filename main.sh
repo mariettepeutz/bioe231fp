15ca17941a227c0490d6f463ab1160ef841332fb
Kate Barouch <131073205+katebarouch@users.noreply.github.com>
2024-12-03 17:59:39 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -143,7 +143,8 @@ process_comparison_genome() {
 # Process DENV-1 Genomes
 process_reference_genome "DENV-1" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.fna.gz" 
 process_reference_annotation_file "DENV-1" "denv1genes.gff3"
-# process_reference_annotation "DENV-1" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.gff.gz"
+process_reference_annotation "DENV-1" "https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/862/125/GCF_000862125.1_ViralProj15306/GCF_000862125.1_ViralProj15306_genomic.gff.gz"
+
 process_comparison_genome "DENV-1" \
     "DENV-1_New_Caledonia-2017-AVS-NC-094" "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=MW315194.1&report=fasta&format=text" 
 process_comparison_genome "DENV-1" \
